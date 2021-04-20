7d6c0e3fd98da830eacf7f6189ce9ad68dd06f3f
Overfirst <overfirst@inbox.ru>
2021-04-20 23:06:00 +0300

    Auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../shared/services/auth.service";
+import { Subscription } from "rxjs";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   public form: FormGroup;
+  private aSub: Subscription;
 
-
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -23,9 +30,28 @@ export class LoginPageComponent implements OnInit {
         Validators.minLength(6)
       ])
     });
+
+    this.route.queryParams.subscribe( (params: Params) => {
+      if (params['registered']) {
+
+      } else if (params['accessDenied']) {
+        
+      }
+    } );
   }
 
-  onSubmit(): void {
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
+  }
 
+  public onSubmit(): void {
+    this.form.disable();
+
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      () => this.form.enable()
+    );
   }
 }
---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../interfaces";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private token: string = null;
+
+  constructor(private http: HttpClient) {}
+
+  public register(user: User): void {
+
+  }
+
+  public login(user: User): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>('/api/auth/login', user)
+      .pipe(
+        tap( ({ token }) => {
+          localStorage.setItem('auth-token', token);
+        })
+      );
+  }
+
+  private setToken(token: string): void {
+    this.token = token;
+  }
+
+  public getToken(): string {
+    return this.token;
+  }
+
+  public isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  public logout(): void {
+    this.setToken(null);
+    localStorage.clear();
+  }
+}
